80886d35fb89de88ff2a94e30089c3ebaff82d0c
Roman <tyniv@progroma.com>
2021-07-11 23:33:21 +0300

    global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Header from './components/Header'
+
+// Styles
+import { GlobalStyle } from './GlobalStyles';
+
+
+
 function App() {
   return (
     <div className="App">
-      Start here.
+      <Header />
+      <GlobalStyle />
     </div>
   );
 }
